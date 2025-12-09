5539783c3d193bc40873f714125da4290bc80327
andkwhyu <andika.ramadhan@students.amikom.ac.id>
2025-12-09 14:55:59 +0700

    Update Toggle

--- obj/Debug/net8.0/MVC_1.AssemblyInfo.cs
+++ obj/Debug/net8.0/MVC_1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MVC_1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+246ba706f1ce1c6c067b15ff1dbc7d6c427cfb1c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33ceb1aa0b875f107686cf1fba46968838293a22")]
 [assembly: System.Reflection.AssemblyProductAttribute("MVC_1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MVC_1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
